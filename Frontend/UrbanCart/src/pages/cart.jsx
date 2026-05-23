afde751f41830aa7cfcf5ee6b59738fbb2216bd5
komal kori <korikomal15@gmail.com>
2026-05-23 13:42:00 +0530

    implement cart summary with subtotal, shipping and grand total

--- Frontend/UrbanCart/src/pages/cart.jsx
+++ Frontend/UrbanCart/src/pages/cart.jsx
@@ -1,114 +1,138 @@
-    import React from 'react'
+import React from 'react'
 
-    import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 
-    import { getCart, updateCart } from '../redux/thunks/cartThunks'
-    import { useEffect } from 'react'
-
-
+import { getCart, updateCart } from '../redux/thunks/cartThunks'
+import { useEffect } from 'react'
 
 
 
-    const CartPage = () => {
-
-        const dispatch = useDispatch()
-        const { items, loading, error } = useSelector((state) => state.cart)
-        // console.log("cart data", items)
-
-        useEffect(() => {
-            dispatch(getCart())
-        }, [dispatch])
-
-        // if (loading) {
-        //     return <h2>Loading...</h2>
-        // }
-        if (error) {
-            return <h2>{error}</h2>
-        }
-        return (
-            <>
 
 
-                <div>
+const CartPage = () => {
 
-                    <h1>My Cart</h1>
+    const dispatch = useDispatch()
+    const { items, loading, error } = useSelector((state) => state.cart)
+    // console.log("cart data", items)
 
-                    {
-                        items?.map((item) => (
+    useEffect(() => {
+        dispatch(getCart())
+    }, [dispatch])
 
-                            <div
-                                key={item.product._id}
-                            >
-
-                                <img
-                                    src={item.product.images[0]?.url}
-                                    alt={item.product.title}
-                                    width="120"
-                                />
-
-                                <h3>
-                                    {item.product.title}
-                                </h3>
-
-                                <p>
-                                    ₹ {item.product.price}
-                                </p>
-
-                                <button
-                                    type="button"
-
-                                    onClick={() =>
-                                        dispatch(
-                                            updateCart({
-
-                                                productId:
-                                                    item.product._id,
-
-                                                action: "decrease"
-                                            })
-                                        )
-                                    }
-
-                                >
-                                    -
-                                </button>
-
-
-
-                                <span>
-                                    {item.quantity}
-                                </span>
-
-
-
-                                <button
-                                    type="button"
-                                    onClick={() =>
-                                        dispatch(
-                                            updateCart({
-
-                                                productId:
-                                                    item.product._id,
-
-                                                action: "increase"
-                                            })
-                                        )
-                                    }
-
-                                >
-                                    +
-                                </button>
-
-                            </div>
-                        ))
-                    }
-
-                </div>
-
-
-
-            </>
-        )
+    // if (loading) {
+    //     return <h2>Loading...</h2>
+    // }
+    if (error) {
+        return <h2>{error}</h2>
     }
 
-    export default CartPage
+    const totalItem = items.reduce((acc, item) => {
+        return acc + item.quantity
+    },
+        0)
+
+
+    const subTotal = items.reduce((acc, item) => {
+
+        return acc + (
+            item.quantity * item.product.price
+        )
+    },
+        0)
+
+    const shipping = subTotal >= 5000 ? 0 : 100
+
+    const grandTotal = subTotal + shipping
+    return (
+        <>
+
+
+            <div>
+
+                <h1>My Cart</h1>
+
+                <p>Total Items:{totalItem} </p>
+                <p>subTotal: {subTotal}</p>
+                <p>shipping: {shipping}</p>
+                <h3>Grand Total :{grandTotal}</h3>
+
+
+                {
+                    items?.map((item) => (
+
+                        <div
+                            key={item.product._id}
+                        >
+
+                            <img
+                                src={item.product.images[0]?.url}
+                                alt={item.product.title}
+                                width="120"
+                            />
+
+                            <h3>
+                                {item.product.title}
+                            </h3>
+
+                            <p>
+                                ₹ {item.product.price}
+                            </p>
+
+                            <button
+                                type="button"
+
+                                onClick={() =>
+                                    dispatch(
+                                        updateCart({
+
+                                            productId:
+                                                item.product._id,
+
+                                            action: "decrease"
+                                        })
+                                    )
+                                }
+
+                            >
+                                -
+                            </button>
+
+
+
+                            <span>
+                                {item.quantity}
+                            </span>
+
+
+
+                            <button
+                                type="button"
+                                onClick={() =>
+                                    dispatch(
+                                        updateCart({
+
+                                            productId:
+                                                item.product._id,
+
+                                            action: "increase"
+                                        })
+                                    )
+                                }
+
+                            >
+                                +
+                            </button>
+
+                        </div>
+                    ))
+                }
+
+            </div>
+
+
+
+        </>
+    )
+}
+
+export default CartPage
