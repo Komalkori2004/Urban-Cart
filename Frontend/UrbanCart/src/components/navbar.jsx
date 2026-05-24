278be69e256bde21947704b513f1d1a935a97871
komal kori <korikomal15@gmail.com>
2026-05-24 22:05:01 +0530

    create luxury centered navbar ui for urbancart

--- Frontend/UrbanCart/src/components/navbar.jsx
+++ Frontend/UrbanCart/src/components/navbar.jsx
@@ -1,37 +1,75 @@
-
+import './style/navbar.css'
 
 import React from 'react'
 
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import {useLocation} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 const NavBar = () => {
 
-  const location=useLocation()
-  if(location.pathname.includes("/admin")){
+  const location = useLocation()
+  if (location.pathname.includes("/admin")) {
     return null
   }
 
-    const {items=[]}=useSelector((state)=>state.cart)
+  const { items = [] } = useSelector((state) => state.cart)
 
-    const cartCount=items.reduce((acc,item)=>{
-        return acc+item.quantity
-    },0)
+  const cartCount = items.reduce((acc, item) => {
+    return acc + item.quantity
+  }, 0)
   return (
-   <>
-   
-   <nav>
+    <>
 
-    <Link to="/">Home</Link>
+      <nav className="navbar">
 
-    <Link to="/cart">Cart {cartCount}</Link>
-   </nav>
-   
-   
-   
-   
-   </>
+        <div className="nav-links">
+
+          <Link to="/">
+            Home
+          </Link>
+
+          <Link to="/products">
+            Shop
+          </Link>
+
+
+
+          <div className="nav-logo">
+
+            <Link to="/">
+
+              <img
+                src="/logo/nav-logo.png"
+                alt="UrbanCart"
+              />
+
+            </Link>
+
+          </div>
+
+
+
+          <Link to="/wishlist">
+            Wishlist
+          </Link>
+
+          <Link to="/cart">
+            Cart ({cartCount})
+          </Link>
+
+          <Link to="/profile">
+            Profile
+          </Link>
+
+        </div>
+
+      </nav>
+
+
+
+
+    </>
   )
 }
 
