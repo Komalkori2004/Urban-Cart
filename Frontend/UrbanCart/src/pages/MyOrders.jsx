6cc4017def89066a873e08b4d63f854c02b2bd41
komal kori <korikomal15@gmail.com>
2026-06-03 21:27:01 +0530

    add complete order details modal for users

--- Frontend/UrbanCart/src/pages/MyOrders.jsx
+++ Frontend/UrbanCart/src/pages/MyOrders.jsx
@@ -80,6 +80,75 @@ const MyOrders = () => {
                         >
                             Close
                         </button>
+                        <h3>Customer Information</h3>
+
+                        <p>
+                            <strong>Name:</strong>{" "}
+                            {selectedOrder.shippingAddress?.fullName}
+                        </p>
+
+                        <p>
+                            <strong>Phone:</strong>{" "}
+                            {selectedOrder.shippingAddress?.phone}
+                        </p>
+
+                        <hr />
+
+                        <h3>Shipping Address</h3>
+
+                        <p>
+                            {selectedOrder.shippingAddress?.addressLine1}
+                        </p>
+
+                        <p>
+                            {selectedOrder.shippingAddress?.addressLine2}
+                        </p>
+
+                        <p>
+                            {selectedOrder.shippingAddress?.city},
+                            {" "}
+                            {selectedOrder.shippingAddress?.state}
+                        </p>
+
+                        <p>
+                            {selectedOrder.shippingAddress?.pincode}
+                        </p>
+
+                        <p>
+                            {selectedOrder.shippingAddress?.country}
+                        </p>
+
+                        <hr />
+                        <h3>Products</h3>
+
+                        {
+                            selectedOrder?.items?.map((item) => (
+
+                                <div
+                                    key={item._id}
+                                    className="order-product"
+                                >
+                                    <img
+                                        src={item.image}
+                                        alt={item.name}
+                                        width="80"
+                                    />
+
+                                    <div>
+                                        <h4>{item.name}</h4>
+
+                                        <p>
+                                            Qty: {item.quantity}
+                                        </p>
+
+                                        <p>
+                                            ₹{item.price}
+                                        </p>
+                                    </div>
+
+                                </div>
+                            ))
+                        }
 
                         <h3>Order Details</h3>
 
