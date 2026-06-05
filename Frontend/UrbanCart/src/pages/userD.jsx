6d1b0417a101d9a909f7ee5eb07c0a31ac0c2768
komal kori <korikomal15@gmail.com>
2026-06-05 22:03:28 +0530

    add user profile dashboard UI

--- Frontend/UrbanCart/src/pages/userD.jsx
+++ Frontend/UrbanCart/src/pages/userD.jsx
@@ -4,38 +4,75 @@ import React, { useEffect, useState } from 'react'
 import { getProfile } from '../redux/thunks/authThunks'
 import { useDispatch, useSelector } from 'react-redux'
 
-
+import "../style/userD.css"
 
 const UserProfile = () => {
 
   const dispatch = useDispatch()
-  const { user,loading,error } = useSelector(state => state.auth)
+  const { user, loading, error } = useSelector(state => state.auth)
 
   useEffect(() => {
     dispatch(getProfile())
   }, [dispatch])
 
-  console.log("user ",user)
-    return (
-        <>
+  console.log("user ", user)
+  return (
+    <>
 
-<div>
-  <h1>User Profile</h1>
+      <div className="profile-container">
 
-  <h3>Name : {user?.name}</h3>
+        <div className="profile-card">
 
-  <h3>Email : {user?.email}</h3>
+          <div className="profile-header">
 
-  <h3>Role : {user?.role}</h3>
+            <div className="profile-avatar">
+              {user?.name?.charAt(0).toUpperCase()}
+            </div>
 
-  <h3>
-    Verified :
-    {user?.isVerified ? " Yes" : " No"}
-  </h3>
-</div>
+            <h2>{user?.name}</h2>
 
-        </>
-    )
+            <p className="profile-tag">
+              Premium Member
+            </p>
+
+          </div>
+
+          <div className="profile-item">
+            <span>Name</span>
+            <h3>{user?.name}</h3>
+          </div>
+
+          <div className="profile-item">
+            <span>Email</span>
+            <h3>{user?.email}</h3>
+          </div>
+
+          <div className="profile-item">
+            <span>Role</span>
+            <h3>{user?.role}</h3>
+          </div>
+
+          <div className="profile-item">
+            <span>Status</span>
+
+            {
+              user?.isVerified ? (
+                <div className="verified-badge">
+                  Verified
+                </div>
+              ) : (
+                <div className="unverified-badge">
+                  Not Verified
+                </div>
+              )
+            }
+          </div>
+
+        </div>
+
+      </div>
+    </>
+  )
 }
 
 export default UserProfile
