119af90d595f045c92e0f6613ab069ee4370e5b0
komal kori <korikomal15@gmail.com>
2026-06-05 23:37:11 +0530

    add single product page UI

--- Frontend/UrbanCart/src/pages/singleProduct.jsx
+++ Frontend/UrbanCart/src/pages/singleProduct.jsx
@@ -5,11 +5,16 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { getsingleProduct } from '../redux/thunks/productThunks'
 
+import {useNavigate} from "react-router-dom"
+
+import "../style/singleProduct.css"
+
 
 const SingleProduct = () => {
+    const navigate = useNavigate()
 
     const dispatch = useDispatch()
-    const {slug} = useParams()
+    const { slug } = useParams()
 
     const { singleProduct, loading, error } = useSelector((state) => state.products)
     useEffect(() => {
@@ -17,7 +22,7 @@ const SingleProduct = () => {
 
     }, [slug, dispatch])
 
-    console.log(singleProduct);
+
 
 
     if (loading) {
@@ -30,34 +35,75 @@ const SingleProduct = () => {
     return (
         <>
 
-            <div className="single-product">
-                <img src={singleProduct.images?.[0]?.url} alt={singleProduct.name} width={400} />
+            <div className="single-product-container">
 
-                <div>
+                <div className="single-product-image">
 
-                    <h2>{singleProduct.name}</h2>
-                    <p>{singleProduct.description}</p>
-                    <h3>${singleProduct.price}</h3>
-                    <p>{singleProduct.category}</p>
-                    <p>
-                        Brand:
-                        {singleProduct?.brand}
-                    </p>
-
-                    <p>
-                        Stock:
-                        {singleProduct?.stock}
-                    </p>
-
-<button>Add to Cart</button>
-
+                    <img
+                        src={singleProduct.images?.[0]?.url}
+                        alt={singleProduct.name}
+                    />
 
                 </div>
 
+                <div className="single-product-content">
 
+                    <p className="product-label">
+                        Luxury Collection
+                    </p>
 
+                    <h1>
+                        {singleProduct.name}
+                    </h1>
 
+                    <p className="product-brand">
+                        {singleProduct.brand}
+                    </p>
 
+                    <h2 className="product-price">
+                        ₹ {singleProduct.price}
+                    </h2>
+
+                    <p className="product-description">
+                        {singleProduct.description}
+                    </p>
+
+                    <div className="product-meta">
+
+                        <span>
+                            Category:
+                            {singleProduct.category}
+                        </span>
+
+                        <span
+                            className={
+                                singleProduct.stock > 0
+                                    ? "in-stock"
+                                    : "out-stock"
+                            }
+                        >
+                            {
+                                singleProduct.stock > 0
+                                    ? "In Stock"
+                                    : "Out Of Stock"
+                            }
+                        </span>
+
+                    </div>
+
+                    <div className="product-actions">
+
+                        <button className="add-cart-btn">
+                            Add To Cart
+                        </button>
+
+                        <button className="buy-now-btn" >
+                            Buy Now
+                        </button>
+
+                    </div>
+
+                </div>
 
             </div>
 
