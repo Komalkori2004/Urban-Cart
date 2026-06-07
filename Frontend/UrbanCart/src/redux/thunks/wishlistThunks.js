4d357696ab6760e20ea7d828d9186548a21159d0
komal kori <korikomal15@gmail.com>
2026-06-07 12:03:06 +0530

    Implement wishlist add get and remove thunks

--- Frontend/UrbanCart/src/redux/thunks/wishlistThunks.js
+++ Frontend/UrbanCart/src/redux/thunks/wishlistThunks.js
@@ -23,5 +23,56 @@ export const addToWishlist = createAsyncThunk(
         } catch (error) {
             return thunkAPI.rejectWithValue(error.response.data.message)
         }
+        
     }
+)
+
+export const getWishlist=createAsyncThunk(
+    "wishlist/getWishlist",
+
+
+    async(_,thunkAPI)=>{
+        try{
+            const token =thunkAPI.getState().auth.token
+            const config={
+                headers:{
+                    Authorization:`Bearer ${token}`
+                }
+            }
+            const {data}=await api.get("/wishlist",config)
+            return data
+
+        }
+        catch(error){
+            return thunkAPI.rejectWithValue(error.response.data.message)
+        }
+    }
+)
+
+
+export const removeWishlist=createAsyncThunk(
+    "wishlist/removeWishlist",
+    
+    async(productId,thunkAPI)=>{
+        
+        try{
+
+            const token =thunkAPI.getState().auth.token
+            const config={
+                headers:{
+                    Authorization:`Bearer ${token}`
+                }
+            }
+            const {data}=await api.delete(`/wishlist/${productId}`,config)
+            return data
+
+        }
+
+        catch(error){
+            return thunkAPI.rejectWithValue(error.response.data.message)
+        }
+    }
+
+
+
 )
