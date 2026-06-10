7ce327538489424bd23261c1f04e86419a48bed4
komal kori <korikomal15@gmail.com>
2026-06-10 16:34:32 +0530

    feat: add address management with profile integration

--- Frontend/UrbanCart/src/pages/userD.jsx
+++ Frontend/UrbanCart/src/pages/userD.jsx
@@ -3,94 +3,277 @@
 import React, { useEffect, useState } from 'react'
 import { getProfile } from '../redux/thunks/authThunks'
 import { useDispatch, useSelector } from 'react-redux'
-import { getAddresses } from '../redux/thunks/authThunks'
+import { getAddresses ,addAddress} from '../redux/thunks/authThunks'
 
 import "../style/userD.css"
 
 const UserProfile = () => {
 
   const dispatch = useDispatch()
-  const { user, loading, error,addresses } = useSelector(state => state.auth)
+
+  const { user, loading, error, addresses } = useSelector(state => state.auth)
+  const [showAddresses, setShowAddresses] = useState(false);
+  const [shippingAddress, setShippingAddress] = useState({
+  fullName: "",
+  phone: "",
+  addressLine1: "",
+  addressLine2: "",
+  city: "",
+  state: "",
+  pincode: "",
+  country: "India"
+})
 
   useEffect(() => {
     dispatch(getProfile())
     dispatch(getAddresses())
   }, [dispatch])
 
+      const handleChange = (e) => {
+
+        setShippingAddress({
+            ...shippingAddress, [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = async (e) => {
+  e.preventDefault()
+
+  const resultAction =
+  await dispatch(
+    addAddress(shippingAddress)
+  )
+
+  console.log(shippingAddress)
+
+  if(addAddress.fulfilled.match(resultAction)){
+    
+    dispatch(getAddresses())
+
+    setShowAddresses(false)
+
+    setShippingAddress({
+      fullName:"",
+      phone:"",
+      addressLine1:"",
+      addressLine2:"",
+      city:"",
+      state:"",
+      pincode:"",
+      country:"India"
+    })
+}
+}
   console.log("user ", user)
   return (
     <>
-    <div className="container">
+      <div className="container">
 
-      <div className="profile-container">
+        <div className="profile-container">
 
-        <div className="profile-card">
+          <div className="profile-card">
 
-          <div className="profile-header">
+            <div className="profile-header">
 
-            <div className="profile-avatar">
-              {user?.name?.charAt(0).toUpperCase()}
+              <div className="profile-avatar">
+                {user?.name?.charAt(0).toUpperCase()}
+              </div>
+
+              <h2>{user?.name}</h2>
+
+              <p className="profile-tag">
+                Premium Member
+              </p>
+
             </div>
 
-            <h2>{user?.name}</h2>
+            <div className="profile-item">
+              <span>Name</span>
+              <h3>{user?.name}</h3>
+            </div>
 
-            <p className="profile-tag">
-              Premium Member
-            </p>
+            <div className="profile-item">
+              <span>Email</span>
+              <h3>{user?.email}</h3>
+            </div>
+
+            <div className="profile-item">
+              <span>Role</span>
+              <h3>{user?.role}</h3>
+            </div>
+
+            <div className="profile-item">
+              <span>Status</span>
+
+              {
+                user?.isVerified ? (
+                  <div className="verified-badge">
+                    Verified
+                  </div>
+                ) : (
+                  <div className="unverified-badge">
+                    Not Verified
+                  </div>
+                )
+              }
+            </div>
 
           </div>
+          <h2>Saved Addresses</h2>
 
-          <div className="profile-item">
-            <span>Name</span>
-            <h3>{user?.name}</h3>
-          </div>
+          {
+            addresses?.map((address) => (
+              <div key={address._id}>
+                <h4>{address.fullName}</h4>
 
-          <div className="profile-item">
-            <span>Email</span>
-            <h3>{user?.email}</h3>
-          </div>
+                <p>{address.phone}</p>
 
-          <div className="profile-item">
-            <span>Role</span>
-            <h3>{user?.role}</h3>
-          </div>
+                <p>{address.addressLine1}</p>
 
-          <div className="profile-item">
-            <span>Status</span>
+                <p>{address.city}</p>
 
-            {
-              user?.isVerified ? (
-                <div className="verified-badge">
-                  Verified
-                </div>
-              ) : (
-                <div className="unverified-badge">
-                  Not Verified
-                </div>
-              )
-            }
-          </div>
+                <p>{address.state}</p>
+              </div>
+            ))
+          }
+        
 
         </div>
-        <h2>Saved Addresses</h2>
+          <button
+            className="add-address-btn"
+            onClick={() => setShowAddresses(true)}
+          >
+            + Add New Address
+          </button>
 
-{
-  addresses?.map((address) => (
-    <div key={address._id}>
-      <h4>{address.fullName}</h4>
 
-      <p>{address.phone}</p>
+          {
+          showAddresses && (
+              <div>
 
-      <p>{address.addressLine1}</p>
+                <div className="checkout-container">
+<h1 className="checkout-title">
+  Add New Address
+</h1>
 
-      <p>{address.city}</p>
+            <form
+                className="checkout-form"
+                onSubmit={handleSubmit}
+            >
 
-      <p>{address.state}</p>
-    </div>
-  ))
-}
+                <input
+                    type="text"
+                    required
+                    placeholder="Full Name"
+                    name="fullName"
+                    onChange={handleChange}
+                    value={shippingAddress.fullName}
+                />
 
-      </div>
+                <input
+                    type="text"
+                    placeholder="Phone Number"
+                    name="phone"
+                    onChange={handleChange}
+                    value={shippingAddress.phone}
+                    required
+                />
+
+                <input
+                    type="text"
+                    placeholder="Address Line 1"
+                    name="addressLine1"
+                    className="full-width"
+                    onChange={handleChange}
+                    value={shippingAddress.addressLine1}
+                    required
+                />
+
+                <input
+                    type="text"
+                    placeholder="Address Line 2"
+                    className="full-width"
+                    name="addressLine2"
+                    onChange={handleChange}
+                    value={shippingAddress.addressLine2}
+                />
+
+                <input
+                    type="text"
+                    placeholder="City"
+                    name="city"
+                    onChange={handleChange}
+                    value={shippingAddress.city}
+                    required
+                />
+
+                <input
+                    type="text"
+                    placeholder="State"
+                    name="state"
+                    onChange={handleChange}
+                    value={shippingAddress.state}
+                    required
+                />
+
+                <input
+                    type="text"
+                    placeholder="Pincode"
+                    name="pincode"
+                    onChange={handleChange}
+                    value={shippingAddress.pincode}
+                    required
+                />
+
+
+                <input
+                    type="text"
+                    placeholder="Country"
+                    name="country"
+                    value={shippingAddress.country}
+                    onChange={handleChange}
+                    readOnly
+                />
+                {/* <div className="payment-section">
+
+                    <h3>Payment Method</h3>
+
+                    <label className="payment-option">
+
+                        <input
+                            type="radio"
+                            value="COD"
+                            checked={paymentMethod === "COD"}
+                            onChange={(e) => setPaymentMethod(e.target.value)}
+                        />
+
+                        <span>Cash On Delivery</span>
+
+                    </label>
+                    <label className="payment-option disabled">
+
+                        <input
+                            type="radio"
+                            value="ONLINE"
+                            disabled
+                        />
+
+                        <span>Online Payment (Coming Soon)</span>
+
+                    </label>
+                </div> */}
+
+            <button type="submit">
+  Save Address
+</button>
+
+            </form>
+
+        </div >
+
+              </div>
+            )
+          }
       </div>
     </>
   )
