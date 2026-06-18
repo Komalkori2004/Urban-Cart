853b3788306ce451fdc33c872336d51dd6b64e18
komal kori <korikomal15@gmail.com>
2026-06-18 17:28:52 +0530

    feat: create contact schema

---
+++ Backend/models/contactModel.js
@@ -0,0 +1,41 @@
+
+
+
+const mogoose = require("mongoose")
+const contactSchema = new mogoose.Schema({
+    
+         
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    subject: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+
+},{timestamps:true})
+
+
+module.exports=mogoose.model("Contact",contactSchema)
