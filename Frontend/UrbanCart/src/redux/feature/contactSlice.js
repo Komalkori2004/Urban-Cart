46cc3a449db6d1e227fe6b344d8937d707da3744
komal kori <korikomal15@gmail.com>
2026-06-18 18:40:24 +0530

    feat: add contact redux slice

---
+++ Frontend/UrbanCart/src/redux/feature/contactSlice.js
@@ -0,0 +1,90 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import {
+    createContact,
+    getContacts,
+    markAsRead,
+} from "../thunks/contactThunk";
+
+
+const initialState = {
+    loading: false,
+    contacts: [],
+    success: false,
+    message: "",
+    error: null,
+};
+
+
+
+
+const contactSlice = createSlice({
+    name: "contact",
+    initialState,
+    reducers: {
+        resetContactState: (state) => {
+            state.loading = false;
+            state.success = false;
+            state.message = "";
+            state.error = null;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(createContact.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+                state.success = false;
+            })
+            .addCase(createContact.fulfilled, (state, action) => {
+                state.loading = false;
+                state.error = null;
+                state.success = true;
+                state.message = action.payload.message;
+            })
+            .addCase(createContact.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+                state.success = false;
+            })
+
+            .addCase(getContacts.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+                state.success = false;
+            })
+
+            .addCase(getContacts.fulfilled, (state, action) => {
+                state.loading = false;
+                state.error = null;
+                state.success = true;
+                state.contacts = action.payload.contacts;
+            })
+            .addCase(getContacts.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+                state.success = false;
+            })
+            .addCase(markAsRead.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+                state.success = false;
+            })
+            .addCase(markAsRead.fulfilled, (state, action) => {
+                state.loading = false;
+                state.error = null;
+                state.success = true;
+                state.message = action.payload.message;
+            })
+            .addCase(markAsRead.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+                state.success = false;
+            })
+    },
+});
+
+export const { resetContactState } = contactSlice.actions;
+export default contactSlice.reducer;
+
+
