af87cb0bf01ef578865fb91c038461f6dab8a10f
komal kori <korikomal15@gmail.com>
2026-07-12 17:20:54 +0530

    feat(profile): add profile image upload with Cloudinary

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -7,6 +7,32 @@ const asyncHandler = require("../middleware/asyncHandler");
 const ErrorHandler = require("../utils/errorHandler");
 
 const bcrypt = require("bcryptjs");
+const streamifier = require("streamifier");
+const cloudinary = require("../config/cloudinary");
+
+
+const uploadToCloudinary = async (fileBuffer) => {
+    return new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream(
+            {
+                folder: "urbancart_users",
+            },
+            (error, result) => {
+                if (error) {
+                    reject(error)
+                }
+                else {
+                    resolve(result)
+                }
+            }
+
+
+        )
+        streamifier.createReadStream(fileBuffer).pipe(stream)
+
+    })
+
+}
 
 
 
@@ -151,9 +177,58 @@ const changePassword = asyncHandler(async (req, res, next) => {
 });
 
 
+
+
+
+const uploadProfileImage = asyncHandler(async (req, res, next) => {
+
+    const existingUser = await User.findById(req.user.id);
+
+    if (!existingUser) {
+        return next(
+            new ErrorHandler(404, "User not found")
+        );
+    }
+
+    if (!req.file) {
+        return next(
+            new ErrorHandler(400, "Please upload a profile image")
+        );
+    }
+
+    if (existingUser.avatar.public_id) {
+        await cloudinary.uploader.destroy(
+            existingUser.avatar.public_id
+        );
+    }
+
+    const result = await uploadToCloudinary(
+        req.file.buffer
+    );
+
+
+    existingUser.avatar.public_id = result.public_id;
+
+    existingUser.avatar.url = result.secure_url;
+
+
+
+    await existingUser.save();
+    existingUser.password = undefined;
+    res.status(200).json({
+        success: true,
+        message: "Profile image uploaded successfully",
+        user: existingUser,
+    });
+
+});
+
+
+
 module.exports = {
 
 
     updateProfile,
     changePassword,
+    uploadProfileImage
 }
--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -3,17 +3,24 @@
 
 const express =require("express")
 const router=express.Router()
+const upload = require("../middleware/multer")
 
 
 
 const { authMiddleware, authorizeRoles } = require("../middleware/authMiddlewar")
 
-const {updateProfile, changePassword } = require("../controller/userController")
+const {updateProfile, changePassword,uploadProfileImage } = require("../controller/userController")
 
 
 
 router.put("/profile",authMiddleware,updateProfile)
 router.put("/change-password",authMiddleware,changePassword)
+router.put(
+    "/profile-image",
+    authMiddleware,
+    upload.single("avatar"),
+    uploadProfileImage
+);
 
 
 
